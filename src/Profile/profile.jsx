4d7a098b14b4c85f2d70148ab68f2f111dccf989
egoriktomatoes <tchepasovegor@gmail.com>
2024-04-22 22:14:41 +0500

    upd 1.0.1

--- src/Profile/profile.jsx
+++ src/Profile/profile.jsx
@@ -1,36 +1,96 @@
-import React from "react";
+import React, {useState, useEffect} from 'react'
 import {
 	BrowserRouter as Router,
 	Routes,
 	Route,
-	Link,
 	Navigate,
 	useLocation,
 } from 'react-router-dom'
 import Userfront, {
 	SignupForm,
 	LoginForm,
-	PasswordResetForm,
+	PasswordResetForm,LogoutButton
 } from '@userfront/toolkit/react'
 
+import { Form, Link } from 'react-router-dom'
 
-export default function Profile(){
 
-    return(
-        <RequireAuth>
-        <div>
-            {Userfront.user.email}
-        </div>
-        </RequireAuth>
-    )
+
+export async function loader({params}){
+	console.log(1, params);
+    return null;
 }
 
+export async function action(params){
+	console.log(2, params);
+    return null;
+}
+
+
+export default function Profile() {
+	const data = { }
+	return (
+		<RequireAuth>
+			<div>
+				<div>
+					<img key={data.image} src={data.image} alt='User photo' />
+				</div>
+				<div>First name: {data.name}</div>
+				<div>Second name: {data?.surname}</div>
+				<div>Email: {data.email}</div>
+				<div>Age: {data?.age}</div>
+				<div>Country: {data?.country}</div>
+				<div>City: {data?.city}</div>
+				<div>Street: {data?.street}</div>
+				<div>House: {data?.house}</div>
+				<div>Apartment: {data?.apartment}</div>
+				<div>Phone: {data?.phone}</div>
+				<Form action='edit'>
+					<button>EDIT</button>
+				</Form>
+				<Form method='post'>
+					<input type='text' name='simple' placeholder='something' />
+					<button type='submit'>Update</button>
+				</Form>
+				<LogoutButton
+					theme={{
+						colors: {
+							light: '#ffffff',
+							dark: '#5e72e4',
+							accent: '#13a0ff',
+							lightBackground: '#fdfdfd',
+							darkBackground: '#2d2d2d',
+						},
+						colorScheme: 'auto',
+						fontFamily: 'Avenir, Helvetica, Arial, sans-serif',
+						size: 'compact',
+						extras: {
+							rounded: true,
+							gradientButtons: true,
+							hideSecuredMessage: true,
+						},
+					}}
+				/>
+				;
+			</div>
+		</RequireAuth>
+	)
+}
 
 function RequireAuth({ children }) {
 	let location = useLocation()
 	if (!Userfront.tokens.accessToken) {
 		return <Navigate to='/login' state={{ from: location }} replace />
 	}
-
 	return children
+}
+
+
+async function callBackend() {
+	const response = await fetch('/express_backend')
+    const data = await response.json()
+    if (response.status!== 200){
+		throw Error(data.message)
+	}
+	return data
 }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login_pages from './login/login_page';
-import Profile from './Profile/profile';
-import Signup_pages from './login/sign_up_page';
+import Login_pages from './login/login_page'
+import Profile from './Profile/profile'
+import Signup_pages from './login/sign_up_page'
+import {  loader as ProfileLoader, action as ProfileAction } from './Profile/profile'
 
 const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<App />
-  },
-  {
-    path: "/login",
-    element: <Login_pages />
-  },
-  {
-    path: "/profile",
-    element: <Profile />
-  },
-  {
-    path: "/signup",
-    element: <Signup_pages />
-  }
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/login',
+		element: <Login_pages />,
+	},
+	{
+		path: '/profile/:id',
+		element: <Profile />,
+		loader: ProfileLoader,
+		action: ProfileAction,
+	},
+	{
+		path: '/signup',
+		element: <Signup_pages />,
+	},
 ])
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
@@ -38,4 +40,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
